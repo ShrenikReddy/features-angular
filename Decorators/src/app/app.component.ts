5b9c7debbb6de44a2868bbb79e9c97170e01b5d4
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-21 18:01:34 +0530

    in-built pipes

--- Decorators/src/app/app.component.ts
+++ Decorators/src/app/app.component.ts
@@ -11,5 +11,11 @@ import { HighlightElementDirective } from './highlight-element.directive';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-    
+    employees = [
+      { name: 'John', age: 25 },
+      { name: 'Jane', age: 30 },
+      { name: 'Bob', age: 35 },
+      { name: 'Alice', age: 20 },
+
+];
 }
