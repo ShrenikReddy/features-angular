0cd325de0881320451a3e1b373a071879e870c2c
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-21 14:50:29 +0530

    Decorator inheritance

---
+++ Decorators/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from "./child/child.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ChildComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  displayMessage: string = 'Messag from parent component';
+
+  showMsg(){
+    this.displayMessage = 'Child component - Message updated';
+  }
+}
