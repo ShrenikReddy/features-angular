bc682cf4de7bf4d39810e59e52b64f8c07a04f18
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-22 15:20:43 +0530

    Router Link

--- Decorators/src/app/app.component.ts
+++ Decorators/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewChecked, Component, ViewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child/child.component';
 import { CommonModule, JsonPipe } from '@angular/common';
 import { HighlightElementDirective } from './highlight-element.directive';
@@ -7,7 +7,7 @@ import { SortNumbersPipe } from './sort-numbers.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChildComponent,CommonModule, SortNumbersPipe],
+  imports: [RouterOutlet, ChildComponent,CommonModule, SortNumbersPipe, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
