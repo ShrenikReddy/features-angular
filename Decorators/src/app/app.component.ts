cb716b80748803f6fe9f19f13c1df5205b288e1a
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-22 12:03:44 +0530

    Number pipe

--- Decorators/src/app/app.component.ts
+++ Decorators/src/app/app.component.ts
@@ -11,36 +11,11 @@ import { HighlightElementDirective } from './highlight-element.directive';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  products: { name: string; imageUrl: string } = [
-    {
-      name: 'fullstack',
-      imageUrl: 'assets/Bun.jpg ',
-    },
-    {
-      name: 'front-end',
-      imageUrl: 'assets/FullStack.jpg',
-    },
-    {
-      name: 'back-end',
-      imageUrl: 'assets/FullStack.jpg',
-    },
-    {
-      name: 'NestJS',
-      imageUrl: 'assets/NestJS.jpg',
-    };
-  ];
+  weightInPounds: number = 0;
+  weightInKilograms: number =0;
 
-  pgSize: number = 4;
-  startIndex: number = 0;
-  endIndex: number = this.pgSize;
-
-  previousPage(){
-    this.startIndex -= this.pgSize;
-    this.endIndex -= this.pgSize;
+  convertWeight(){
+    this.weightInKilograms = this.weightInPounds * 0.453592;
   }
 
-  nextPage(){
-    this.startIndex += this.pgSize;
-    this.endIndex += this.pgSize;
-  }
 }
