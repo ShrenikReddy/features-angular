8baa4103f9cf1942a7e10cbca35fb57632b0cbb2
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-23 12:42:16 +0530

    Service Providers

--- Services/src/app/app.component.ts
+++ Services/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink,RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ProductsComponent } from './products/products.component';
+import { MyComponentComponent } from "./my-component/my-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, RouterLink, ProductsComponent],
+  imports: [RouterOutlet, FormsModule, RouterLink, ProductsComponent, MyComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
