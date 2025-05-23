a54f1196d37376abb833ee1029ed5d9575df9274
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-23 12:19:38 +0530

    Dependency injection

---
+++ Services/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink,RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule, RouterLink, ProductsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Services';
+}
