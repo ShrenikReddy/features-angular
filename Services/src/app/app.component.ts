f76f846c8999ff94d8e68ed2d54827c086c9711d
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-27 11:48:10 +0530

    Submit and reset form using NgForm

--- Services/src/app/app.component.ts
+++ Services/src/app/app.component.ts
@@ -1,30 +1,37 @@
 import { Component, InjectionToken } from '@angular/core';
-import { RouterLink,RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { ProductsComponent } from './products/products.component';
-import { MyComponentComponent } from "./my-component/my-component.component";
-import { LogMessage1Service } from './services/log-message1.service';
-import { LogMessage2Service } from './log-message2.service';
-import { AlertMessage1Service } from './services/alert-message1.service';
-import { NgForm } from '@angular/forms';
+// import { RouterLink,RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+// import { ProductsComponent } from './products/products.component';
+// import { MyComponentComponent } from "./my-component/my-component.component";
+// import { LogMessage1Service } from './services/log-message1.service';
+// import { LogMessage2Service } from './log-message2.service';
+// import { AlertMessage1Service } from './services/alert-message1.service';
+// import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, RouterLink, ProductsComponent, MyComponentComponent],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [FormsModule]
-
-  
+  imports: [FormsModule],
 })
-export class AppComponent implments OnInit{
-    user: { username: string} = { username:''};
-
-    submitForm(myForm: NgForm) {
-      if(myForm.valid){
-        alert("S  ubmitted the form successfully, check console");
-        console.log(this.user);
-      }
+export class AppComponent {
+  formSubmit(myForm: NgForm) {
+    if (myForm.valid) {
+      const formValues = JSON.stringify(myForm.value);
+      console.log(formValues);
+      console.log('%c Form submitted successfully', 'color:green');
+    } else {
+      alert('Please fill all the fields');
     }
+  }
+  resetValue(myForm: NgForm) {
+    myForm.resetForm();
+  }
+  setDefault(myForm: NgForm) {
+    myForm.resetForm({
+      name: 'Default Name',
+      email: 'defaultId@gmail.com',
+    });
+  }
 }
-
