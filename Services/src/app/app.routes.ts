6323b8521f06d87a29fdfe6557e7e1a3509a5323
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-29 11:49:57 +0530

    Deferred loading

--- Services/src/app/app.routes.ts
+++ Services/src/app/app.routes.ts
@@ -8,6 +8,10 @@ export const routes: Routes = [
     // { path: 'login', component: LoginComponent },
     // { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] }, 
     // { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'first', loadComponent() => import('./components/first/first.component') },
+    { path: 'first', 
+      loadComponent() => import('./components/first/first.component'),
+      
+
+    },
     { path: 'second', loadComponent() => import('./components/second/second.component').then(m => m.SecondComponent), }
 ];
