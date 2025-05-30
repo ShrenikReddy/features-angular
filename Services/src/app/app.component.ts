1b40525180bb70a010f2f7cfd14cb29295976dc3
ShrenkReddy <shrenik.reddy@wenable.com>
2025-05-30 16:01:34 +0530

    RxJS { EMPTY } operator

--- Services/src/app/app.component.ts
+++ Services/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { interval, timer} from "rxjs";
+import { EMPTY } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,16 @@ import { interval, timer} from "rxjs";
 })
 export class AppComponent implements OnInit{
   ngOnInit(): void {
-    const timerObs$ = timer(2000,1000);
-    timerObs$.subscribe((val) => console.log('Value: ${val}'));
+    const src1 = of(1,2,3);
+    const src2 = EMPTY;
+
+    src1.subscribe({
+      next: (value) => console.log('Source 1 Next:', value),
+      complete: () => console.log('Source 1 Complete'),
+    });
+     
+    src2.subscribe({
+      next: (value) => console.log('Source 2 Next:', value),
+      complete: () => console.log('Source 2 Complete'),
   }
 }
